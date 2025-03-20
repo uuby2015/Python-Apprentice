814ae318b505f1b41e9127ecf3719eff930ae72f
uuby2015 <nicknicknickstudent@gmail.com>
2025-03-20 00:01:44 +0000

    i learned how to change the picture on the screen

--- lessons/00_Turtles/08c_More_Turtle_Programs.py
+++ lessons/00_Turtles/08c_More_Turtle_Programs.py
@@ -7,3 +7,30 @@ from the section "Change the Background Image"
 Then change the code so that the turtle has a different image ( look in the 'images'
 directory ) and moves to the corners of the screen in a square pattern. 
 """
+import turtle
+
+def set_background_image(window, image_name):
+    """Set the background image of the turtle window to the image with the given name."""
+
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+# Set up the screen
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(width=600, height=600)     # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+screen = turtle.Screen()                # Get the screen that tina is on
+set_background_image(screen, "moustache1.gif") # Set the background image of the screen
+
+turtle.exitonclick()      
