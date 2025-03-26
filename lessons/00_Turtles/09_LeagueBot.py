220d4f00032a841f74c562f039e57c460e4b0435
uuby2015 <nicknicknickstudent@gmail.com>
2025-03-26 23:57:40 +0000

    I learned how to change the image

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -8,13 +8,33 @@ Write your own turtle program! Here is what your program should do
 4) Draw a hexagon using a loop and variables. 
 
 """
+import turtle
 
-import turtle as turtle
+screen = turtle.getscreen()
 
+screen.setup(width=1000, height=1000)
+
+
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+   
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+# Set up the screen
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor('white')
 
-t = turtle.Turtle()
+# Create a turtle and set its shape to the custom GIF
+dog = turtle.Turtle()
 
-... # Your Code Here
+set_turtle_image(dog, 'leaguebot_bolt.gif')
+
+
+dog.speed(3)
+turtle.done()
--- lessons/00_Turtles/10c_Tash_Me_Twirl.py
+++ lessons/00_Turtles/10c_Tash_Me_Twirl.py
@@ -9,3 +9,56 @@ Hint: See 08a_More Turtle Programs, section 'Click on the Turtle'
 
 ... # Your code here
 
+""" Tash Me
+
+Write a program that:
+1) Loads an emoji image as the background
+2) Make the turtle shape a moustach
+3) Move the moustache to the right spont on the emoji
+
+Hint: See 08a_More Turtle Programs, section 'Change the Background Image' and
+'Change the Turtle Shape'
+"""
+
+... # Your code here
+""" Leaguebot
+
+Write your own turtle program! Here is what your program should do
+
+1) Change the turtle image to 'leaguebot_bolt.gif'
+2) Change the turtle size to 10x10
+3) Change the turtle line color to 'blue'
+4) Draw a hexagon using a loop and variables. 
+
+"""
+import turtle
+
+screen = turtle.getscreen()
+
+screen.setup(width=1000, height=1000)
+
+
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+   
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+# Set up the screen
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+# Create a turtle and set its shape to the custom GIF
+dog = turtle.Turtle()
+
+set_turtle_image(dog, 'moustache1.gif')
+
+
+dog.speed(3)
+turtle.done()
+
